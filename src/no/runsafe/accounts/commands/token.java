f93e376b063d1a4b63281f13e8c04b8e3739eed6
Kruithne <kruithne@gmail.com>
2013-05-10 19:39:43 +0100

    Fixed IntelliJ derp

---
+++ src/no/runsafe/accounts/commands/token.java
@@ -0,0 +1,25 @@
+package no.runsafe.accounts.commands;
+
+import no.runsafe.accounts.Engine;
+import no.runsafe.framework.command.player.PlayerCommand;
+import no.runsafe.framework.server.player.RunsafePlayer;
+
+import java.util.HashMap;
+
+public class Token extends PlayerCommand
+{
+	public Token(Engine engine)
+	{
+		super("token", "Generates a new account token", "runsafe.accounts.token");
+		this.engine = engine;
+	}
+
+	@Override
+	public String OnExecute(RunsafePlayer executor, HashMap<String, String> parameters)
+	{
+		executor.sendColouredMessage("&cDO NOT give your token to ANYONE under any circumstance.");
+		return "&3New account token: " + this.engine.getNewAuthToken(executor);
+	}
+
+	private Engine engine;
+}
