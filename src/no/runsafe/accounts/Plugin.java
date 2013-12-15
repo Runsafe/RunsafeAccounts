05ef86d1a26f2c13378d33b1c149753f4a602cea
Morten Nilsen <morten@runsafe.no>
2013-12-15 12:52:31 +0100

    Declare features used

--- src/no/runsafe/accounts/Plugin.java
+++ src/no/runsafe/accounts/Plugin.java
@@ -4,12 +4,18 @@ import no.runsafe.accounts.commands.Token;
 import no.runsafe.accounts.repositories.AccountRepository;
 import no.runsafe.framework.RunsafePlugin;
 import no.runsafe.framework.api.command.Command;
+import no.runsafe.framework.features.Commands;
+import no.runsafe.framework.features.Database;
 
 public class Plugin extends RunsafePlugin
 {
 	@Override
 	protected void PluginSetup()
 	{
+		// Framework features
+		addComponent(Database.class);
+		addComponent(Commands.class);
+
 		// Repositories
 		this.addComponent(AccountRepository.class);
 
