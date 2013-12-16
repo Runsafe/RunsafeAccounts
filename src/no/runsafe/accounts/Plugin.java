0c338947b948e5274b4a231c554df4cbc2226797
Morten Nilsen <morten@runsafe.no>
2013-12-16 16:31:24 +0100

    renamed upstream

--- src/no/runsafe/accounts/Plugin.java
+++ src/no/runsafe/accounts/Plugin.java
@@ -10,7 +10,7 @@ import no.runsafe.framework.features.Database;
 public class Plugin extends RunsafePlugin
 {
 	@Override
-	protected void PluginSetup()
+	protected void pluginSetup()
 	{
 		// Framework features
 		addComponent(Database.class);
